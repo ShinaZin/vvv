20fbc246bf97a1ce7f8ba36fda25831c6f0e55d0
ShinaZin <ShinaZin@mail.ru>
2023-10-23 22:13:07 +0300

    MAIN: basic gravity code implemented

--- src/main.c
+++ src/main.c
@@ -1,89 +1,70 @@
 #include <genesis.h>
 #include <resources.h>
 
-u16 ind = TILE_USER_INDEX;
-float fg_offset = 0;
+u16 tileIdx = TILE_USER_INDEX;
+float bgOffset = 0;
 
 Sprite *player;
-u16 player_x = 120;
-u16 player_y = 80;
-// u16 key_prev = 0;
+u16 playerX = 120;
+u16 playerY = 100;
+s16 playerGravity = 4;
 
-#define ANIM_STILL 0
-#define ANIM_IDLE 1
-#define ANIM_WALK 2
-#define ANIM_UPPER 3
+#define ANIM_IDLE 0
+#define ANIM_WALK 1
+#define ANIM_HURT 2
 
-static void handleInput()
-{
-    u16 key = JOY_readJoypad(JOY_1);
-    u8 speed = 1;
-    bool isNotBeatingUpper =
-        player->animInd != ANIM_UPPER ||
-        player->animInd == ANIM_UPPER && player->frameInd > 5;
-    bool isWalking = key & BUTTON_LEFT || key & BUTTON_RIGHT || key & BUTTON_UP || key & BUTTON_DOWN;
+#define LINE_PADDING 45
+#define LINE_TOP_Y 0 + LINE_PADDING
+#define LINE_BOTTOM_Y 200 - LINE_PADDING
 
-    if (key & BUTTON_LEFT && isNotBeatingUpper)
-    {
-        player_x -= speed;
-        fg_offset += 0.3;
-        SPR_setHFlip(player, FALSE);
-    }
-    else if (key & BUTTON_RIGHT && isNotBeatingUpper)
-    {
-        player_x += speed;
-        fg_offset -= 0.3;
-        SPR_setHFlip(player, TRUE);
-    }
-    if (key & BUTTON_UP && isNotBeatingUpper && player_y > 70)
-    {
-        player_y -= speed;
-    }
-    else if (key & BUTTON_DOWN && isNotBeatingUpper && player_y < 100)
-    {
-        player_y += speed;
-    }
+static void handleInput() {
+  u16 key = JOY_readJoypad(JOY_1);
+  u8 speed = 2;
+  bool isWalking = key & BUTTON_LEFT || key & BUTTON_RIGHT;
 
-    // animations
-    if (isNotBeatingUpper)
-    {
-        SPR_setAnim(player, isWalking ? ANIM_WALK : ANIM_IDLE);
-    }
-    if (key & BUTTON_A)
-    {
-        SPR_setAnim(player, ANIM_UPPER);
-    }
-
-    SPR_setPosition(player, player_x, player_y);
-    // foreground
-    VDP_setHorizontalScroll(BG_A, fg_offset);
+  if (key & BUTTON_LEFT) {
+    playerX -= speed;
+    SPR_setHFlip(player, TRUE);
+  } else if (key & BUTTON_RIGHT) {
+    playerX += speed;
+    SPR_setHFlip(player, FALSE);
+  }
+  // animations
+  SPR_setAnim(player, isWalking ? ANIM_WALK : ANIM_IDLE);
+  SPR_setPosition(player, playerX, playerY);
 }
 
+static void handleGravity() {
+  bool isInverting = playerY > LINE_BOTTOM_Y || playerY < LINE_TOP_Y;
+  if (isInverting) {
+    playerGravity *= -1;
+  }
+  playerY += playerGravity;
+  SPR_setVFlip(player, playerGravity < 0);
+}
 
-int main()
-{
-    PAL_setPalette(PAL0, bg1.palette->data, DMA);
-    VDP_drawImageEx(BG_B, &bg1, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, ind), 0, 0, FALSE, TRUE);
-    ind += bg1.tileset->numTile;
+int main() {
+  // PAL_setPalette(PAL1, i_bg1.palette->data, DMA);
+  VDP_drawImageEx(BG_B, &i_bg1,
+                  TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, tileIdx + 1), 0, 0,
+                  FALSE, TRUE);
 
-    PAL_setPalette(PAL1, fg1.palette->data, DMA);
-    VDP_drawImageEx(BG_A, &fg1, TILE_ATTR_FULL(PAL1, 0, FALSE, FALSE, ind), 0, -2, FALSE, TRUE);
-    ind += fg1.tileset->numTile;
+  VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
 
-    VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
+  SPR_init();
+  PAL_setPalette(PAL2, s_player.palette->data, DMA);
+  player = SPR_addSprite(&s_player, playerX, playerY,
+                         TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
 
-    SPR_init();
-    PAL_setPalette(PAL2, my_sprite.palette->data, DMA);
-    player = SPR_addSprite(&my_sprite, player_x, player_y, TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
-    SPR_setAnim(player, ANIM_WALK);
+  while (1) {
+    handleInput();
+    handleGravity();
+    // bg scroll
+    bgOffset += 2;
+    VDP_setVerticalScroll(BG_B, bgOffset);
 
-    while (1)
-    {
-        VDP_drawText("Demo by pzinovev", 0, 0);
-        handleInput();
-
-        SPR_update();
-        SYS_doVBlankProcess();
-    }
-    return (0);
+    SPR_update();
+    SYS_doVBlankProcess();
+  }
+  return (0);
 }
